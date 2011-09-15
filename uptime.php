757f48154e8b4a079a6be4796844c467c93a380f
jfoucher <jfoucher@6px.eu>
2011-09-15 12:15:37 +0200

    first commit

---
+++ uptime.php
@@ -0,0 +1,109 @@
+<?php
+
+/* 
+SCHEMA
+
+
+
+CREATE TABLE IF NOT EXISTS `monitor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `load1` float NOT NULL,
+  `load5` float NOT NULL,
+  `load15` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
+*/
+
+define('SERVER','localhost');
+define('USER','user');
+define('PASSWORD','pass');
+define('DATABASE','db');
+define('TABLE','table');
+
+
+$c=mysql_connect(SERVER,USER,PASSWORD);
+mysql_select_db(DATABASE);
+
+if($_GET['savetodb']){
+	/*
+	ob_start();
+	$test = system('uptime');
+	ob_end_clean();
+*/
+	$loads = sys_getloadavg();
+
+	//$uptime=trim(substr($test,-5,5));
+	print_r($loads);
+
+	$q= "INSERT INTO `".DATABASE."`.`".TABLE."` (`timestamp`, `load1`, `load5`, `load15`) 
+		VALUES ( CURRENT_TIMESTAMP, '".$loads[0]."', '".$loads[1]."', '".$loads[2]."');";
+
+	mysql_query($q); 
+}else{
+
+
+
+	//get data from mysql
+
+	$q= "SELECT * FROM `".DATABASE."`.`".TABLE."`;";
+
+	$r=mysql_query($q); 
+	$data='';
+	while ($row = mysql_fetch_object($r)){
+
+		list($date,$time)=explode(' ',$row->timestamp);
+		list($y,$m,$d)=explode('-',$date);
+		list($h,$i,$s)=explode(':',$time);
+		$load1=$row->load1;
+		$load5=$row->load5;
+		$load15=$row->load15;
+		$data.="{c:[{v: new Date($y, $m, $d,$h,$i,$s)}, {v: $load1}, {v: $load5}, {v: $load15}]},";
+	}
+	//generate and show graph
+
+	?>
+	<html>
+	<head>
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	    <script type="text/javascript">
+	    
+	      // Load the Visualization API and the piechart package.
+	      google.load('visualization', '1.0', {'packages':['corechart','annotatedtimeline','table']});
+	      
+	      // Set a callback to run when the Google Visualization API is loaded.
+	      google.setOnLoadCallback(drawChart);
+
+
+
+	function drawChart() {
+
+	   var data = new google.visualization.DataTable(
+	   {
+	     cols: [{id: 'date', label: 'Date', type: 'date'},
+		    {id: 'load1', label: 'Load 1 min', type: 'number'},
+			{id: 'load5min', label: 'Load 5 min', type: 'number'},
+			{id: 'load15min', label: 'Load 15 min', type: 'number'}],
+	     rows: [
+		    <?php echo $data ?>
+		   ]
+	   }
+	) 
+
+	var options = {'title':'15 minutes load xiilo.com',
+		             'width':400,'displayAnnotations': true,
+		             'height':300};
+
+	 
+	  var annotatedtimeline = new google.visualization.AnnotatedTimeLine(
+	      document.getElementById('visualization'));
+	  annotatedtimeline.draw(data, options);
+	}
+	</script>
+	</head>
+	<body>
+	<div id="visualization" style="width:600px;height:400px;margin:0 auto"></div>
+	</body>
+	</html>
+<?php
+}
